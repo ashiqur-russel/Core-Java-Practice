2b6e6bfdcac3222b656eb1a6471d033e8d314e19
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-11-23 07:46:44 +0100

    Exercise-2 : Task 2 for University java course ( Do-While Practice )

---
+++ University Course Lecture & Exercise/src/Exercise2/Task3.java
@@ -0,0 +1,38 @@
+/**
+ * Exercise 2 : Task 3
+ * <p>
+ * Write a do-while loop that asks the user to enter two numbers. The numbers should be added and the sum displayed.
+ * The loop should ask the user whether he or she wishes to perform the operation again. If so, the loop should repeat;
+ * otherwise it should terminate.
+ * <p>
+ * Test Data
+ * Enter the number 2
+ * Do you want to continue y/n? y Enter the number 3
+ * Do you want to continue y/n? n
+ * <p>
+ * Expected Output:
+ * Sum of even numbers: 2 Sum of odd numbers: 3
+ */
+
+package Exercise2;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num1 = input.nextInt();
+        System.out.println("Do you wish to continue y/n");
+
+        String wish = input.next();
+
+        if (wish.equals("y")) {
+            System.out.println("Enter Number again");
+        }
+
+    }
+
+
+}
