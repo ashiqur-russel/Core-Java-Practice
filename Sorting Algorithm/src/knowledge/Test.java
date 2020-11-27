def282f7b865f5f1d4f55c28119cad4fbc6f2e6f
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-11-27 02:33:07 +0100

    sorting algorithm knowledge test with selection sort

--- Sorting Algorithm/src/knowledge/Test.java
+++ Sorting Algorithm/src/knowledge/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         int[] num = {-3, 4, 0, 5, 9, 11, 6};
 
+        // test
 
         for (int i = 0; i < num.length - 1; i++) {
             int min = i;
