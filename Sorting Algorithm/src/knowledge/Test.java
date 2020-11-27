b0b4b77a2559cae0d44d85d5a0166e96d2a5ad80
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-11-27 02:30:53 +0100

    Exercise-2 : Task 2 for University java course ( Do-While Practice )

---
+++ Sorting Algorithm/src/knowledge/Test.java
@@ -0,0 +1,28 @@
+package knowledge;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] num = {-3, 4, 0, 5, 9, 11, 6};
+
+
+        for (int i = 0; i < num.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[j] < num[min]) {
+                    min = j;
+                }
+
+                int temp = num[i];
+                num[i] = num[min];
+
+                num[min] = temp;
+
+            }
+            System.out.print(num[i] + " ");
+        }
+
+
+    }
+}
