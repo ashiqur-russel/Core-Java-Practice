03b61a339608b3e1cebcf2262dec49f6877ba483
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-11-29 04:47:05 +0100

    update : Created insert() Method for insert item to the list

--- Array Practice/src/moshLearning/Array.java
+++ Array Practice/src/moshLearning/Array.java
@@ -2,13 +2,21 @@ package moshLearning;
 
 public class Array {
     private int[] items;
+    private int count = 0;
 
     public Array(int itemNo) {
         items = new int[itemNo];
     }
 
+    //Insert item to the Array
+    public void insert(int item) {
+        items[count] = item;
+        count++;
+    }
+
+    // Method for print Array
     public void print() {
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < count; i++) {
             System.out.println(items[i]);
         }
     }
--- Array Practice/src/moshLearning/Main.java
+++ Array Practice/src/moshLearning/Main.java
@@ -3,5 +3,12 @@ package moshLearning;
 public class Main {
     public static void main(String[] args) {
         Array item = new Array(3);
+
+        item.insert(3);
+        item.insert(2);
+        item.insert(13);
+
+
+        item.print();
     }
 }
