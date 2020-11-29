22d78c11ab3dfd9d2ee0a38d297194f0665e0044
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-11-29 04:38:40 +0100

    Created a variable where item will be saved , a constructor ot pass the size of array, a method to print the Array list inserted and Array object to the main class.

--- Array Practice/src/moshLearning/Array.java
+++ Array Practice/src/moshLearning/Array.java
@@ -1,4 +1,16 @@
 package moshLearning;
 
 public class Array {
+    private int[] items;
+
+    public Array(int itemNo) {
+        items = new int[itemNo];
+    }
+
+    public void print() {
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
 }
--- Array Practice/src/moshLearning/Main.java
+++ Array Practice/src/moshLearning/Main.java
@@ -2,6 +2,6 @@ package moshLearning;
 
 public class Main {
     public static void main(String[] args) {
-
+        Array item = new Array(3);
     }
 }
