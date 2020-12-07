cb44e66a21f4c18f9198bfbc39d1fde3b739290c
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-12-07 23:40:24 +0100

    Task 3 completed with all the error validation

--- University Assignment/src/Assignment4/Assignment4.java
+++ University Assignment/src/Assignment4/Assignment4.java
@@ -1,14 +1,28 @@
 package Assignment4;
 
 public class Assignment4 {
+
+    // Main method
+
     public static void main(String[] args) {
         int[] values = {9, -3, 5, 0, 1};
 
         int[] array = {1, 1, 2, 3, 11, 12, 13, 24, 38, 55};
 
-        System.out.println(find(array, 4));
 
-        //sort(values);
+        /* check all the validation for code is ok for task 3 */
+        System.out.println("Array is [ 1, 1, 2, 3, 11, 12, 13, 24, 38, 55 ]");
+        ;
+        System.out.println(" find index of  0 :" + find(array, -1));
+        System.out.println(" find index of  0 :" + find(array, 0));
+        System.out.println(" find index of  1 :" + find(array, 1));
+        System.out.println(" find index of  2 :" + find(array, 2));
+        System.out.println(" find index of  4 :" + find(array, 4));
+        System.out.println(" find index of  7 :" + find(array, 7));
+        System.out.println(" find index of  44 :" + find(array, 44));
+        System.out.println(" find index of  55 :" + find(array, 55));
+        System.out.println(" find index of  59 :" + find(array, 59));
+
 
     }
 
@@ -43,6 +57,7 @@ public class Assignment4 {
 
     }
 
+
     //Task 3
 
     public static int find(int[] numbers, int numberToCheck) {
@@ -64,6 +79,8 @@ public class Assignment4 {
                 return middle;
             }
         }
+        if (numberToCheck > numbers[left])
+            left = left + 1;
         return -(left + 1);
 
     }
