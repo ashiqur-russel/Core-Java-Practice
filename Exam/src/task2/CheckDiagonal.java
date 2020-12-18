92ab630decf11468a5e8f7363fa2a46a1f6a4f92
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-12-18 23:02:04 +0100

    University Mock Test task check diagonal in array

---
+++ Exam/src/task2/CheckDiagonal.java
@@ -0,0 +1,21 @@
+package task2;
+
+public class CheckDiagonal {
+
+    public static void main(String[] args) {
+        int[][] M = {{1, 0, 0, 0, 0}, {0, 2, 0, 0, 0}, {0, 0, 3, 0, 0}, {0, 0, 0, 2, 0}, {0, 0, 0, 0, 4}};
+        System.out.println(checkDiagonal(M));
+
+    }
+
+
+    public static Boolean checkDiagonal(int M[][]) {
+        for (int i = 0; i < M.length; i++)
+            for (int j = 0; j < M.length; j++) {
+                if ((i != j) && M[i][j] != 0) {
+                    return false;
+                }
+            }
+        return true;
+    }
+}
