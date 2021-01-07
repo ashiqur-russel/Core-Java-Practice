7ec826d50bf3397a971de20a1cc44b2d39f175b0
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2021-01-07 03:26:43 +0100

    created web controller

--- Test Spring Boot Project/src/main/java/com/testproject/WebController.java
+++ Test Spring Boot Project/src/main/java/com/testproject/WebController.java
@@ -1,8 +1,18 @@
 package com.testproject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
+
 public class WebController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        System.out.println("Hello Spring Boot Project");
+        return "hello";
+
+    }
+
 }
