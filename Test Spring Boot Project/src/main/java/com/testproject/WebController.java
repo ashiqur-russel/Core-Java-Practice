d3c5fb085339951fdf67f323a32e0d1bf43c8e7d
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2021-01-07 03:38:23 +0100

    Test project done and compiled successfully

--- Test Spring Boot Project/src/main/java/com/testproject/WebController.java
+++ Test Spring Boot Project/src/main/java/com/testproject/WebController.java
@@ -1,18 +1,21 @@
 package com.testproject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
-
 public class WebController {
 
     @RequestMapping("/hello")
-    public String sayHello() {
-        System.out.println("Hello Spring Boot Project");
+
+    public String sayHello(Model model) {
+
+        System.out.println("Hello Spring Boot");
+
+        model.addAttribute("message", "Greeting from Ashiqur Russel");
         return "hello";
 
     }
-
 }
+
