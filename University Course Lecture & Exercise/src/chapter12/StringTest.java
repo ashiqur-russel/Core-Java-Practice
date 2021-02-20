c2b8479963ebf9d596d10efee75bee113449c958
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2021-02-20 19:52:02 +0100

    String comparison with == & equal

--- University Course Lecture & Exercise/src/chapter12/StringTest.java
+++ University Course Lecture & Exercise/src/chapter12/StringTest.java
@@ -1,4 +1,18 @@
 package chapter12;
 
 public class StringTest {
+    public static void main(String[] args) {
+        System.out.println(stringTest());
+    }
+
+    public static String stringTest() {
+        String result = " ";
+
+        for (int i = 1; i <= 5; i++) {
+            result = result + 1;
+
+        }
+        return result;
+    }
+
 }
