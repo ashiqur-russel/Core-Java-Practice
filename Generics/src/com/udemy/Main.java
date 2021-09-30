00c7a0a17f0ac2cdd1d7c8dc476859c2105da1ab
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-01 01:25:05 +0200

    Basic Generics Implementation - Object Autoboxing

--- Generics/src/com/udemy/Main.java
+++ Generics/src/com/udemy/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList items = new ArrayList();
+        ArrayList<Integer> items = new ArrayList<>();
         items.add(1);
         items.add(2);
         items.add(3);
@@ -14,9 +14,9 @@ public class Main {
 
     }
 
-    public static void printDoubles(ArrayList n) {
-        for (Object i : n) {
-            System.out.println((Integer) i * 2);
+    public static void printDoubles(ArrayList<Integer> n) {
+        for (int i : n) {
+            System.out.println(i * 2);
         }
     }
 
